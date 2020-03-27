16c9162d69361c801fd574b27e95a33318c17f80
vudu79 <vudu1979@gmail.com>
2020-03-27 22:11:21 +1200

    SpringMongoDB

---
+++ src/main/java/ru/spingmongodb/Application.java
@@ -0,0 +1,19 @@
+package ru.spingmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String showHW() {
+        return "hello-world";
+    }
+}
